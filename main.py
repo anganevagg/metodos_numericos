d379d3e854054f9c0234da7bb3ba6111f6530ca3
Anganevagg <anganevagg@protonmail.com>
2022-04-06 18:20:08 -0500

    todo funciona

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from metodos.euler import metodo_euler
 from metodos.interpolacion import metodo_interpolacion
 from metodos.raphson_newton import metodo_raphson_newton
 
+metodo_euler()
--- metodos/euler.py
+++ metodos/euler.py
@@ -6,17 +6,16 @@ from sympy.abc import x, y
 import numpy as np
 import matplotlib as math
 
+
 def metodo_euler():
 	f = Function("f")
 
 	x0=float(input("x0 = "))
 	y0=float(input("y0 = "))
 	xf=float(input("xf = "))
-
 	h=float(input("h = "))
-	yn=y0
 
-	n=int((xf)/h)+1
+	n=int((xf-x0)/h)+1
 
 	func = input("y = ")
 
@@ -24,23 +23,13 @@ def metodo_euler():
 
 	xv = np.zeros(n)
 	yv = np.zeros(n)
-	ys = np.zeros(n)
-
-	dy=dsolve(sympy.Eq(f(x).diff(x), func.subs(y, f(x))))
-	print(sympy.Eq(f(x).diff(x), func.subs(y, f(x))))
-
-	cte=solve(dy.subs(f(x), y0).subs(x, x0))
-
-	print(dy.lhs)
-	print(cte)
-
 	xv[0] = x0
 	yv[0] = y0
 
-	for i in range(1, n):
-		xv[i] = xv[0]+i*h
-		#print(dy.subs(x, xv[i]).subs(c1, cte[0]))
-		print(dy)
 
-	print(xv)
-	print(yv)
+	for i in range(1, n):
+		xv[i] =	xv[0] + h*i
+		yv[i] = yv[i-1] + h*func.subs(x, xv[i-1]).subs(y, yv[i-1])
+
+
+	print(f"y = {yv[-1]}")
