72a14cb0a3f31b302cfecb0c879c4c37ed4611c0
EmmanuelVega001 <72900109+EmmanuelVega001@users.noreply.github.com>
2022-04-06 21:50:49 -0500

    menu pitero uwu

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from metodos.euler import metodo_euler
 from metodos.interpolacion import metodo_interpolacion
 from metodos.raphson_newton import metodo_raphson_newton
+print("=======================Bienvenido a la calculadora de metodos numericos de LAS RATAS===========================")
 
-metodo_euler()
+print('Metodo de Euler:1\n')
+print('Metodo de Interpolacion:2\n')
+print('Metodo de Newton Raphson:3\n')
+opcion=int(input("Selecciona el metodo \n"))
+
+
+if opcion == 1:
+        print('Metodo de Euler')
+        metodo_euler()
+else:
+    if opcion == 2:
+            print('Metodo de Interpolacion')
+            metodo_interpolacion()
+    else:
+        if opcion == 3:
+                print('Metodo de Newton Raphson')
+                metodo_raphson_newton()
+        else:
+         print('error')
